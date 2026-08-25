73b744228139045aa07037c63499610cd31b850d
pkay3892 <artbypkay@gmail.com>
2026-08-25 23:36:36 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "gap-v1";
+var CACHE = "gap-v2";
 var FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(e){
